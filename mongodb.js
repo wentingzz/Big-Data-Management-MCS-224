d578a516ec65b6c57318093d0ebe0ddab94d8b35
Wenting <wenttzzheng@gmail.com>
2023-10-26 11:24:42 -0700

    Adds installation of Assignment3. issue #3

---
+++ mongodb.js
@@ -0,0 +1,28 @@
+//Insert those to mongodb
+//[{
+//"created_at": "Wed Oct 10 20:19:24 +0000 2022",
+//"id": 1050118621198921728,
+//"text": "hey this is a nice day",
+//"user": {"username": "bob123" }
+//},
+//{
+//"created_at": "Wed Oct 10 20:43:24 +0000 2022",
+//"id": 1050118621198921729,
+//"text": "it is rainy today",
+//"location": "Irvine",
+//"user": {"username": "alice123" }
+//},
+//{
+//"created_at": "Wed Oct 10 20:49:24 +0000 2022",
+//"id": 1050118621198921730,
+//"text": "Where is everyone?",
+//"location": "Los Angeles",
+//"user": {"username": "bob123" }
+//},
+//{
+//"created_at": "Wed Oct 11 21:49:24 +0000 2022",
+//"id": 1050118621198921731,
+//"text": "So many things to do!",
+//"location": "Irvine",
+//"user": {"username": "bob123" }
+//}]
